d7ee1b289528e8701c99cff884abc2ef92c3a127
DouglasATsouza <alanaedouglas2@gmail.com>
2023-10-10 09:29:04 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch new-branch-0910
     Your branch is up to date with 'origin/new-branch-0910'.
    
     Changes to be committed:
    	modified:   src/pages/home/Header.jsx
    	new file:   src/utils/rocketseatLogo.jsx

---
+++ src/utils/rocketseatLogo.jsx
@@ -0,0 +1,6 @@
+const rocketseatLogo = {
+  rocketseatLogoUrl:
+    "https://github.com/douglassouza-src/Exemplo-RocketSeat/raw/main/src/assets/img/rocketseat.png",
+};
+
+export default rocketseatLogo;
